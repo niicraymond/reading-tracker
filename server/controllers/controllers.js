36c36f6f9cc99675dfa11ab213e4963f16d94e4f
Nicole Raymond <nicoleraymond@hotmail.com>
2025-07-22 21:15:43 +0100

    sets up library controller

---
+++ server/controllers/controllers.js
@@ -0,0 +1,9 @@
+const {getLibrary} = require('../models/models')
+
+async function fetchLibrary(req, res) {
+    const userId = req.userId
+    const books = await getLibrary(userId)
+    res.json(books)
+}
+
+module.exports = {fetchLibrary}
