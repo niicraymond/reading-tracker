7fe42ca03893942bd687d9038628b2ea09637eec
Nicole Raymond <nicoleraymond@hotmail.com>
2025-07-22 20:17:25 +0100

    sets up login/auth

---
+++ server/controllers/authController.js
@@ -0,0 +1,28 @@
+const { getUserByEmail } = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+async function login(req, res) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email and Password required" });
+  }
+
+  const user = await getUserByEmail(email);
+  if (!user) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) return res.status(401).json({ error: "Invalid credentials" });
+
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "2h",
+  });
+  return res.json({
+    token,
+    user: { id: user.id, email: user.email, name: user.name },
+  });
+}
+
+module.exports = { login };
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const {login} = require('./controllers/authController')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.post('/api/auth/login', login)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
