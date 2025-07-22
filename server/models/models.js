ab6d815a28bc73061089b61087ae000cb20e3fcb
Nicole Raymond <nicoleraymond@hotmail.com>
2025-07-22 21:10:41 +0100

    sets up library model

---
+++ server/models/models.js
@@ -0,0 +1,15 @@
+const pool = require("../connection");
+
+async function getLibrary(userId) {
+  const result = await pool.query(
+    `SELECT books.*
+    FROM books
+    JOIN user_library 
+        ON books.id = user_library.book_id
+    WHERE user_library.user_id = $1`,
+    [userId]
+  );
+  return result.rows;
+}
+
+module.exports = { getLibrary };
