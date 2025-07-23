dad4b6a5c96783a25d8e92b1eeaea45c4ffecf8e
Nicole Raymond <nicoleraymond@hotmail.com>
2025-07-23 17:07:37 +0100

    adds login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Login from './components/Login'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null)
+
+  function handleLogin(userData){
+    setUser(userData)
+  }
+
+  function handleLogout(){
+    localStorage.removeItem('token');
+    setUser(null)
+  }
+
+  if (!user) {
+    return <Login onLogin={handleLogin}/>
+  }
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <header>
+          <h1>Welcome, {user.name}</h1>
+          <button onClick={handleLogout}>Logout</button>
+        </header>
+        <main>
+          <p>more stuff goes here</p>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import api from "../api";
+
+export default function Login({ onLogin }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await api.post("/auth/login", { email, password });
+      localStorage.setItem("token", res.data.token);
+      onLogin(res.data.user);
+    } catch (err) {
+      setError(err.response?.data?.error || "Login failed");
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+        {error && <div>{error}</div>}
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Log In</button>
+    </form>
+  );
+}
