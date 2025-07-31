0049cdeb5e484315a1ba8eef4133661a98ec20c1
Nicole Raymond <nicoleraymond@hotmail.com>
2025-07-31 11:09:59 +0100

    adds database config

--- server/connection.js
+++ server/connection.js
@@ -4,10 +4,17 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
 
-const config = {
-  connectionString: process.env.DATABASE_URL,
-  ...(ENV === "production" && { max: 2 }),
-};
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 const pool = new Pool(config);
 
