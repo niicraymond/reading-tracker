cd21fb625eb3f55a9c4903a6dafaeaa6c471adc2
Nicole Raymond <nicoleraymond@hotmail.com>
2025-07-31 15:01:14 +0100

    updates api base URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:9090";
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
-})
+  baseURL: `${API_BASE}/api`,
+});
 
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem('token')
-    if (token) config.headers.Authorization = `Bearer ${token}`
-    return config
-})
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-export default api
+export default api;
